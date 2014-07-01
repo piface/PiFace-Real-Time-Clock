27e5c056e2df5e5bc3154636947cd91553b35723
Thomas Preston <thomasmarkpreston@gmail.com>
2014-07-01 11:30:36 +0100

    added comment refering to microchip datasheet

--- install-piface-real-time-clock.sh
+++ install-piface-real-time-clock.sh
@@ -65,6 +65,7 @@ start_on_boot() {
 
     cat >> /etc/rc.local << EOF
 modprobe i2c-dev
+\# Calibrate the clock (default: 0x47). See datasheet for MCP7940N
 i2cset -y $i 0x6f 0x08 0x47
 modprobe i2c:mcp7941x
 echo mcp7941x 0x6f > /sys/class/i2c-dev/i2c-$i/device/new_device
